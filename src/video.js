c205fb97f91eae3b6d3efa3f10b48765152f135d
choi jeong hwan <since740@gmail.com>
2023-09-25 13:58:57 +0900

    Update video.js

--- src/video.js
+++ src/video.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
             return 9;  // 풀사이즈일 때 최대 재생 시간
         } else {
             // 너비가 1200에서 100씩 줄어들 때마다 재생 시간이 1초씩 감소
-            let decrease = Math.floor((1920 - window.innerWidth) / 500);
+            let decrease = Math.floor((1920 - window.innerWidth) / 150);
             return 9 - decrease;
         }
     }
