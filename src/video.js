cf81460f8ee1834b015199e5178f31ec2ea675ff
choi jeong hwan <since740@gmail.com>
2023-09-25 13:52:35 +0900

    Update video.js

--- src/video.js
+++ src/video.js
@@ -2,17 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const titleVideo = document.getElementById("title_video");
 
     function calculateMaxPlayTime() {
-        if (window.innerWidth >= 1500) {
+        if (window.innerWidth >= 1920) {
             return 9;  // 풀사이즈일 때 최대 재생 시간
         } else {
             // 너비가 1200에서 100씩 줄어들 때마다 재생 시간이 1초씩 감소
-            let decrease = Math.floor((1500 - window.innerWidth) / 250);
+            let decrease = Math.floor((1920 - window.innerWidth) / 250);
             return 9 - decrease;
         }
     }
 
     function playOrPauseVideo() {
-        if (window.innerWidth >= 1500) {
+        if (window.innerWidth >= 1920) {
             titleVideo.currentTime = 0;
             titleVideo.play();
         } else if (window.innerWidth < 768) {
