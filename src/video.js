4d7459a5a29b3213bef5518966bfa1f304ca2b01
choi jeong hwan <since740@gmail.com>
2023-09-25 13:02:34 +0900

    Update video.js

--- src/video.js
+++ src/video.js
@@ -1,24 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
     const titleVideo = document.getElementById("title_video");
 
+    function calculateMaxPlayTime() {
+        if (window.innerWidth >= 1200) {
+            return 9;  // 풀사이즈일 때 최대 재생 시간
+        } else {
+            // 너비가 1200에서 100씩 줄어들 때마다 재생 시간이 1초씩 감소
+            let decrease = Math.floor((1200 - window.innerWidth) / 100);
+            return 9 - decrease;
+        }
+    }
+
     function playOrPauseVideo() {
         if (window.innerWidth >= 1200) {
-            titleVideo.currentTime = 0; // 동영상을 처음으로 되돌립니다.
+            titleVideo.currentTime = 0;
             titleVideo.play();
         } else if (window.innerWidth < 768) {
             titleVideo.pause();
         }
     }
 
-    playOrPauseVideo();  // 페이지 로드 시 체크합니다.
+    playOrPauseVideo();
 
-    // 비디오 재생 시간이 5초에 도달하면 멈춤
     titleVideo.addEventListener('timeupdate', function() {
-        if (titleVideo.currentTime >= 3) {
+        let maxPlayTime = calculateMaxPlayTime();
+        if (titleVideo.currentTime >= maxPlayTime) {
             titleVideo.pause();
         }
     });
 
-    // 창 크기 변경 시 동영상 재생/일시정지 처리
     window.addEventListener('resize', playOrPauseVideo);
 });
