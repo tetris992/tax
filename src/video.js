0b86181503442152b1e8c48363b80f9044b8f9c0
choi jeong hwan <since740@gmail.com>
2023-09-27 04:43:41 +0900

    mega revise verson1.5

--- src/video.js
+++ src/video.js
@@ -1,27 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
     const titleVideo = document.getElementById("title_video");
-    const homeSection = document.getElementById("home");
+    const homeTitle = document.querySelector(".home_title");
+    const homeTitleStrong = document.querySelector(".home_title--strong");
+    const homeDescription = document.querySelector(".home_description");
+    const homeContact = document.querySelector(".home_contact");
+    const homeAvatar = document.querySelector(".home_avatar");
+
     
     titleVideo.volume = 0.5;  // 볼륨을 0.5로 설정
-
-    function playOrPauseVideo() {
-        if (window.innerWidth >= 769) {
-            titleVideo.currentTime = 0; // 동영상을 처음으로 되돌립니다.
-            titleVideo.play();
-        } else if (window.innerWidth < 768) {
-            titleVideo.pause();
-        }
-    }
-
-    playOrPauseVideo();  // 페이지 로드 시 체크합니다.
 
     // 비디오 재생 시간이 6.5초에 도달하면 멈춤
     titleVideo.addEventListener('timeupdate', function() {
         if (titleVideo.currentTime >= 6.5) {
             titleVideo.pause();
-            homeSection.classList.add('shrink');
+            
+            // 비디오의 투명도를 서서히 0으로 만드는 애니메이션 적용
+            titleVideo.style.transition = 'opacity 1s ease-out';
+            titleVideo.style.opacity = 0;
+
+            // 글자들이 서서히 보이도록 .fadeIn 클래스 추가
+            homeTitle.classList.add('fadeIn');
+            homeTitleStrong.classList.add('fadeIn');
+            homeDescription.classList.add('fadeIn');
+            homeContact.classList.add('fadeIn');
+            // homeAvatar.classList.add('fadeIn');
+
+            // 애니메이션 종료 후 비디오 요소 삭제
+            setTimeout(() => {
+                titleVideo.remove();
+            }, 1000); // 1초 후
+            // setTimeout(() => {
+            //     homeAvatar.classList.add('fadeIn');
+            // }, 1000);
+            setTimeout(() => {
+                homeAvatar.classList.add('active');
+            }, 2000);
         }
     });
-    // 창 크기 변경 시 동영상 재생/일시정지 처리
-    window.addEventListener('resize', playOrPauseVideo);
 });
