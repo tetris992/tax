3e1c5617923a59a2224b99e15c6372318c3b3d21
choi jeong hwan <since740@gmail.com>
2023-09-30 05:41:33 +0900

    Update bar.js

--- src/bar.js
+++ src/bar.js
@@ -1,16 +1,16 @@
-let animated = false; // 애니메이션이 발생했는지를 확인하는 flag
+let isAnimated = false; // 플래그
 
-document.querySelector('.skills_list').addEventListener('mouseover', function() {
-    // 이미 애니메이션이 한 번 실행되었다면 더 이상 실행하지 않습니다.
-    if (animated) return;
+document.querySelector('.skills_list').addEventListener('mouseover', () => {
+    if (isAnimated) return;
 
-    // 모든 'bar' 요소를 순회
-    document.querySelectorAll('.bar').forEach(function(bar) {
-        // 각 바의 목표 너비를 가져옴
-        let targetWidth = bar.querySelector('.bar_metadata span:last-child').textContent.trim();
-        // 애니메이션 적용 (너비 변경)
-        bar.querySelector('.bar_value').style.width = targetWidth;
+    document.querySelectorAll('.bar').forEach(skillBar => {
+        let targetWidth = skillBar.querySelector('.bar_metadata span:last-child').textContent.trim();
+        skillBar.querySelector('.bar_value').style.width = targetWidth;
     });
 
-    animated = true; // 애니메이션이 실행되었음을 표시
+    isAnimated = true;
+});
+
+window.addEventListener('resize', () => {
+    isAnimated = false;
 });
