2e5d42bb13584513a261ad815159116453d9017f
choi jeong hwan <since740@gmail.com>
2023-09-30 06:26:56 +0900

    Update bar.js

--- src/bar.js
+++ src/bar.js
@@ -1,29 +1,40 @@
 let click = false;
 let resize = false;
 let scroll = false;
+let timeoutId; // setTimeout의 ID를 저장하기 위한 변수
 
-
-// 로직을 별도의 함수로 분리
-window.addEventListener('click', () =>{
-    click = true;
-});
-window.addEventListener('resize', () =>{
-    resize = true;
-});
-window.addEventListener('scroll', () =>{
-    scroll = true;
-});
-
+// 애니메이션 처리 함수
 const handleAnimation = function() {
-    if(!click && !resize && !scroll) 
+    if (!click && !resize && !scroll) {
         document.querySelectorAll('.bar').forEach(skillBar => {
             let targetWidth = skillBar.querySelector('.bar_metadata span:last-child').textContent.trim();
             skillBar.querySelector('.bar_value').style.width = targetWidth;
         });
-}
-    
+    }
+};
 
-setTimeout(handleAnimation, 5000);
+// 5초 후에 애니메이션 실행
+const setAnimationTimeout = function() {
+    timeoutId = setTimeout(handleAnimation, 5000);
+};
+
+setAnimationTimeout(); // 처음에 타이머 설정
+
+// 클릭 이벤트에서 타이머 재설정 로직
+document.addEventListener('click', (e) => {
+    if (e.target === document.documentElement || e.target === document.body) { 
+        clearTimeout(timeoutId); // 이미 설정된 타이머 취소
+        setAnimationTimeout();   // 새로운 타이머 설정
+    }
+});
+
+window.addEventListener('resize', () => {
+    resize = true;
+});
+
+window.addEventListener('scroll', () => {
+    scroll = true;
+});
 
 // let count = 0;
 // const maxRepeats = 5;
