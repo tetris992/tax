3c0123922415be95a31ea017463ec2fd314f4c4d
choi jeong hwan <since740@gmail.com>
2023-09-30 05:47:33 +0900

    Update bar.js

--- src/bar.js
+++ src/bar.js
@@ -1,6 +1,7 @@
-let isAnimated = false; // 플래그
+let isAnimated = false;
 
-document.querySelector('.skills_list').addEventListener('mouseover', () => {
+// 로직을 별도의 함수로 분리
+const handleAnimation = () => {
     if (isAnimated) return;
 
     document.querySelectorAll('.bar').forEach(skillBar => {
@@ -9,8 +10,27 @@ document.querySelector('.skills_list').addEventListener('mouseover', () => {
     });
 
     isAnimated = true;
-});
+};
 
-window.addEventListener('resize', () => {
-    isAnimated = false;
-});
+window.addEventListener('resize', handleAnimation);
+window.addEventListener('scroll', handleAnimation);
+
+
+
+
+// let isAnimated = false; // 플래그
+
+// document.querySelector('.skills_list').addEventListener('mouseover', () => {
+//     if (isAnimated) return;
+
+//     document.querySelectorAll('.bar').forEach(skillBar => {
+//         let targetWidth = skillBar.querySelector('.bar_metadata span:last-child').textContent.trim();
+//         skillBar.querySelector('.bar_value').style.width = targetWidth;
+//     });
+
+//     isAnimated = true;
+// });
+
+// window.addEventListener('resize', () => {
+//     isAnimated = false;
+// });
