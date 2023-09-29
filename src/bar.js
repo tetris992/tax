034d4e7a2d65845cb58ea80c8fd49ee41c6fbd63
choi jeong hwan <since740@gmail.com>
2023-09-30 06:15:59 +0900

    Update bar.js

--- src/bar.js
+++ src/bar.js
@@ -1,41 +1,52 @@
 let click = false;
 let resize = false;
 let scroll = false;
+let animated = false;  // 애니메이션이 실행되었는지 확인하는 flag
 
-
-// 로직을 별도의 함수로 분리
-window.addEventListener('click', () =>{
+// 이벤트 리스너 설정
+window.addEventListener('click', () => {
     click = true;
 });
-window.addEventListener('resize', () =>{
+window.addEventListener('resize', () => {
     resize = true;
 });
-window.addEventListener('scroll', () =>{
+window.addEventListener('scroll', () => {
     scroll = true;
 });
 
+// 애니메이션 처리 함수
 const handleAnimation = function() {
-    if(!click && !resize && !scroll) 
+    if (!animated && !click && !resize && !scroll) {
         document.querySelectorAll('.bar').forEach(skillBar => {
             let targetWidth = skillBar.querySelector('.bar_metadata span:last-child').textContent.trim();
             skillBar.querySelector('.bar_value').style.width = targetWidth;
         });
-}
-    
+        animated = true;
+    }
+};
 
+// 5초 후에 애니메이션 실행
 setTimeout(handleAnimation, 5000);
 
-let count = 0;
-const maxRepeats = 5;
+// 마우스 오버 이벤트에서 애니메이션 재실행 로직
+document.querySelector('.skills_list').addEventListener('mouseover', () => {
+    animated = false;  // 애니메이션 실행 flag 재설정
+    handleAnimation();  // 애니메이션 다시 실행
+});
 
-const intervalId = setInterval(() => {
-    if (count < maxRepeats) {
-        handleAnimation();
-        count++;
-    } else {
-        clearInterval(intervalId); // 횟수 제한에 도달하면 interval을 중지합니다.
-    }
-}, 10000);
+
+// let count = 0;
+// const maxRepeats = 5;
+
+
+// const intervalId = setInterval(() => {
+//     if (count < maxRepeats) {
+//         handleAnimation();
+//         count++;
+//     } else {
+//         clearInterval(intervalId); // 횟수 제한에 도달하면 interval을 중지합니다.
+//     }
+// }, 10000);
 
 
 
