51fdd3aac5b541c0e40b93ad15bdc34285b4c076
choi jeong hwan <since740@gmail.com>
2023-09-30 17:41:25 +0900

    % bar

--- src/bar.js
+++ src/bar.js
@@ -1,23 +1,28 @@
-let isAnimated = false;
-
-// 로직을 별도의 함수로 분리
 const handleAnimation = () => {
     if (isAnimated) return;
 
     document.querySelectorAll('.bar').forEach(skillBar => {
-        let targetWidth = skillBar.querySelector('.bar_metadata span:last-child').textContent.trim();
+        let targetWidthElement = skillBar.querySelector('.bar_metadata span:last-child');
+        let targetWidth = targetWidthElement.textContent.trim();
+        let duration = parseFloat(targetWidth)/100*2;
+        let currentWidth = 0;
+        
+        skillBar.querySelector('.bar_value').style.transition = `width ${duration}s cubic-bezier(0.25, 0.1, 0.25, 1)`;
         skillBar.querySelector('.bar_value').style.width = targetWidth;
+
+        const incrementWidth = () => {
+            currentWidth++;
+            if(currentWidth <= parseFloat(targetWidth)){
+                targetWidthElement.textContent = currentWidth + '%';
+            }else{
+                clearInterval(intervalId); // Stop the interval when reaching the target width
+            }
+        };
+        const intervalId = setInterval(incrementWidth, (duration * 1000) / parseFloat(targetWidth)); // Distributing the duration time for each percentage point
+        
+        // isAnimated = true;
     });
-
-    // isAnimated = true;
 };
-
-window.addEventListener('resize', handleAnimation);
-window.addEventListener('scroll', handleAnimation);
-
-setTimeout(handleAnimation, 4000);
-
-document.getElementById('main').addEventListener('click', handleAnimation);
 
 
 
