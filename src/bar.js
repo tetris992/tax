d0fe70febfd4630d9e6c76e1d73a5fef062a1515
choi jeong hwan <since740@gmail.com>
2023-09-30 23:38:33 +0900

    circle

--- src/bar.js
+++ src/bar.js
@@ -1,3 +1,36 @@
+let isAnimated = false;
+const handleAnimation = () => {
+    if (isAnimated) return;
+
+    document.querySelectorAll('.bar').forEach(skillBar => {
+        let targetWidthElement = skillBar.querySelector('.bar_metadata span:last-child');
+        let targetWidth = targetWidthElement.textContent.trim();
+        let duration = parseFloat(targetWidth)/100*5;
+        let currentWidth = 0;
+        
+        skillBar.querySelector('.bar_value').style.transition = `width ${duration}s cubic-bezier(0.25, 0.1, 0.25, 1)`;
+        skillBar.querySelector('.bar_value').style.width = targetWidth;
+
+        const incrementWidth = () => {
+            currentWidth++;
+            if(currentWidth <= parseFloat(targetWidth)){
+                targetWidthElement.textContent = currentWidth + '%';
+            }else{
+                clearInterval(intervalId); // Stop the interval when reaching the target width
+            }
+        };
+        const intervalId = setInterval(incrementWidth, (duration * 1000) / parseFloat(targetWidth)); // Distributing the duration time for each percentage point
+        
+        isAnimated = true;
+    });
+};
+
+
+window.addEventListener('resize', handleAnimation);
+window.addEventListener('scroll', handleAnimation);
+
+setTimeout(handleAnimation, 4000);
+
 // const handleAnimation = () => {
 //     document.querySelectorAll('.bar').forEach(skillBar => {
 //         let targetWidthElement = skillBar.querySelector('.bar_metadata span:last-child');
@@ -37,36 +70,4 @@
 
 
 
-let isAnimated = false;
-const handleAnimation = () => {
-    if (isAnimated) return;
-
-    document.querySelectorAll('.bar').forEach(skillBar => {
-        let targetWidthElement = skillBar.querySelector('.bar_metadata span:last-child');
-        let targetWidth = targetWidthElement.textContent.trim();
-        let duration = parseFloat(targetWidth)/100*5;
-        let currentWidth = 0;
-        
-        skillBar.querySelector('.bar_value').style.transition = `width ${duration}s cubic-bezier(0.25, 0.1, 0.25, 1)`;
-        skillBar.querySelector('.bar_value').style.width = targetWidth;
-
-        const incrementWidth = () => {
-            currentWidth++;
-            if(currentWidth <= parseFloat(targetWidth)){
-                targetWidthElement.textContent = currentWidth + '%';
-            }else{
-                clearInterval(intervalId); // Stop the interval when reaching the target width
-            }
-        };
-        const intervalId = setInterval(incrementWidth, (duration * 1000) / parseFloat(targetWidth)); // Distributing the duration time for each percentage point
-        
-        isAnimated = true;
-    });
-};
-
-
-window.addEventListener('resize', handleAnimation);
-window.addEventListener('scroll', handleAnimation);
-
-setTimeout(handleAnimation, 4000);
 
