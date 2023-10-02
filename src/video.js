9449510cf10d4bdef5288d4c4a2a85813f4681a4
choi jeong hwan <since740@gmail.com>
2023-10-02 15:27:40 +0900

    주석삭제

--- src/video.js
+++ src/video.js
@@ -7,33 +7,28 @@ document.addEventListener('DOMContentLoaded', function() {
     const homeAvatar = document.querySelector(".home_avatar");
 
 
-    titleVideo.volume = 0.5;  // 볼륨을 0.5로 설정
+    titleVideo.volume = 0.5;  
 
-    // 모바일 환경에서는 비디오 제거
     if (window.innerWidth <= 768) {
         titleVideo.remove();
-        return; // 모바일 환경에서는 아래 로직을 진행하지 않음
+        return; 
     }
 
-    // 비디오 재생 시간이 6.5초에 도달하면 멈춤 및 애니메이션 처리
     titleVideo.addEventListener('timeupdate', function() {
         if (titleVideo.currentTime >= 6.5) {
             titleVideo.pause();
             
-            // 비디오의 투명도를 서서히 0으로 만드는 애니메이션 적용
             titleVideo.style.transition = 'opacity 1s ease-out';
             titleVideo.style.opacity = 0;
 
-            // 글자들이 서서히 보이도록 .fadeIn 클래스 추가
             homeTitle.classList.add('fadeIn');
             homeTitleStrong.classList.add('fadeIn');
             homeDescription.classList.add('fadeIn');
             homeContact.classList.add('fadeIn');
 
-            // 애니메이션 종료 후 비디오 요소 삭제
             setTimeout(() => {
                 titleVideo.remove();
-            }, 1000); // 1초 후
+            }, 1000); 
 
             setTimeout(() => {
                 homeAvatar.classList.add('active');
