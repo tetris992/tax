12ad43d804cb0900a8b6e892f9013b9589056d20
choi jeong hwan <since740@gmail.com>
2023-10-03 20:00:23 +0900

    sidenav

---
+++ src/sidebar.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let sidenav = document.getElementById('mySidenav');
+    document.getElementById('mySidenav').style.opacity = '1';
+    sidenav.style.marginTop = '356px';
+
+})
+
+document.addEventListener('scroll', function(){
+    let sidenav = document.getElementById('mySidenav');
+    if(window.scrollY > 0) {
+        sidenav.style.opacity = '1';
+        // sidenav.style.padding = '0';
+    sidenav.style.marginTop = '356px';
+
+    } 
+});
+
+document.addEventListener('click', function(){
+    let sidenav = document.getElementById('mySidenav');
+    sidenav.style.opacity = '0';
+    // sidenav.style.padding = '0';
+    sidenav.style.marginTop = '0';
+});
+
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.getElementById('mySidenav').style.width = '130px';
+
+// });
+
+// document.addEventListener('scroll', function () {
+//     var sidenav = document.getElementById('mySidenav');
+
+//     if (window.scrollY > 0) {
+//         sidenav.style.opacity = '0.2';
+//     } else {
+//         sidenav.style.opacity = '1';
+//     }
+// });
