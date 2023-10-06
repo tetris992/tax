597ce2a64c5c7098a1f5c58c3c79aaa75d614b8d
choi jeong hwan <since740@gmail.com>
2023-10-06 20:54:02 +0900

    Update bar.js

--- src/bar.js
+++ src/bar.js
@@ -1,28 +1,30 @@
 
-document.querySelectorAll('.bar').forEach(skillBar => {
-    let targetWidthElement = skillBar.querySelector('.bar_metadata span:last-child');
-    let targetWidth = targetWidthElement.textContent.trim();
-    let duration = 15; //
-    
-    skillBar.querySelector('.bar_value').style.transition = `width ${duration}s ease-in-out`;
-    skillBar.querySelector('.bar_value').style.width = targetWidth;
+let isAnimated = false;
+const handleAnimation = () => {
+    if (isAnimated) return;
 
-    // '%'에서 '건'으로 변환: 예를 들어, '55%'를 '550 건'으로 변환
-    let targetCount = parseInt((targetWidth)*10**1.32); // 예를 들어, 소수점이 없는 정수로 변환 후 10을 곱함
-    let currentCount = 1;
-    
-    const incrementCount = () => {
-        currentCount++;
-        if(currentCount <= targetCount){
-            targetWidthElement.textContent = `${currentCount} 건`; // '건' 단위로 표시
-        }else{
-            clearInterval(intervalId); // 목표치에 도달했을 때 인터벌을 정지
-        }
-    };
-    
-    const intervalId = setInterval(incrementCount, (duration * 500) / targetCount); // 동안 각 건수를 등록
-});
+    document.querySelectorAll('.bar').forEach(skillBar => {
+        let targetWidthElement = skillBar.querySelector('.bar_metadata span:last-child');
+        let targetWidth = targetWidthElement.textContent.trim();
+        let duration = parseFloat(targetWidth)/100*15;
+        let currentWidth = 1;
+        
+        skillBar.querySelector('.bar_value').style.transition = `width ${duration}s cubic-bezier(0.25, 0.1, 0.25, 1)`;
+        skillBar.querySelector('.bar_value').style.width = targetWidth;
 
+        const incrementWidth = () => {
+            currentWidth++;
+            if(currentWidth <= parseFloat(targetWidth)){
+                targetWidthElement.textContent = Math.ceil((currentWidth)**2.1) + ' 건';
+            }else{
+                clearInterval(intervalId); // Stop the interval when reaching the target width
+            }
+        };
+        const intervalId = setInterval(incrementWidth, (duration * 1000) / parseFloat(targetWidth)); // Distributing the duration time for each percentage point
+        
+        isAnimated = true;
+    });
+};
 
 
 
