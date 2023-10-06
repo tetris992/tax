43bf5e40d1ce330c269f9ceccc27a5ee57043834
choi jeong hwan <since740@gmail.com>
2023-10-06 22:14:35 +0900

    Update bar.js

--- src/bar.js
+++ src/bar.js
@@ -1,29 +1,29 @@
-let isAnimated = false;
-const handleAnimation = () => {
-    if (isAnimated) return;
+document.addEventListener('DOMContentLoaded', function() {
+    const skillBars = document.querySelectorAll('.bar');
+    skillBars.forEach(skillBar => {
+        let targetWidthText = skillBar.querySelector('.bar_metadata span:last-child').textContent;
+        let targetWidth = parseInt(targetWidthText.replace('건', ''), 10);
+        let currentWidth = 0;
+        let duration = 7;
 
-    document.querySelectorAll('.bar').forEach(skillBar => {
-        let targetWidthElement = skillBar.querySelector('.bar_metadata span:last-child');
-        let targetWidth = targetWidthElement.textContent.trim();
-        let duration = parseFloat(targetWidth)/100*15;
-        let currentWidth = 1;
-        
-        skillBar.querySelector('.bar_value').style.transition = `width ${duration}s cubic-bezier(0.25, 0.1, 0.25, 1)`;
-        skillBar.querySelector('.bar_value').style.width = targetWidth;
+        const updateBarWidth = function() {
+            skillBar.querySelector('.bar_value').style.width = currentWidth + 'px';
+            skillBar.querySelector('.bar_metadata span:last-child').textContent = currentWidth + ' 건';
+        };
 
-        const incrementWidth = () => {
+        const incrementWidth = function () {
             currentWidth++;
-            if(currentWidth <= parseFloat(targetWidth)){
-                targetWidthElement.textContent = Math.ceil((currentWidth)**1.9) + ' 건';
-            }else{
-                clearInterval(intervalId); // Stop the interval when reaching the target width
+            if(currentWidth <= targetWidth) {
+                updateBarWidth();
+            }else {
+                clearInterval(increaseInterval);
             }
         };
-        const intervalId = setInterval(incrementWidth, (duration * 1000) / parseFloat(targetWidth)); // Distributing the duration time for each percentage point
-        
-        isAnimated = true;
+
+        // 계산된 시간 간격으로 바와 숫자를 업데이트 합니다.
+        const increaseInterval = setInterval(incrementWidth, (duration * 1000) / targetWidth);
     });
-};
+});
 
 
 // let isAnimated = false;
